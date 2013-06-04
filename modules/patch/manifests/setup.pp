42ca5ca98157eb6effa590d80c2fb548ebc0a00a
ckesurf <ckesurf@gmail.com>
2013-06-04 14:59:04 -0400

    finished most of setup instructions, including changing hosts file and installing java

--- modules/patch/manifests/setup.pp
+++ modules/patch/manifests/setup.pp
@@ -2,50 +2,14 @@
   
  class patch::setup {
 
-# Postgres
+
+  ############## PostGres ############## 
   include postgresapp
 
   exec {'pg db':
-  	command => "initdb /usr/local/var/postgres -E utf8",
-  	require => Class['postgresapp'],
+    command => "initdb /usr/local/var/postgres -E utf8",
+    require => Class['postgresapp'],
   }
-
-# this works!
-#  include sublime_text_2
-#  sublime_text_2::package { 'Emmet':
-#      source => 'sergeche/emmet-sublime'
-#  }
-
-
-
-  file { '.ivy2':
-    path => "/Users/${boxen_user}/.ivy2",
-    ensure => directory,
-  }
-
-  file { '.credentials':
-    path => "/Users/${boxen_user}/.ivy2/.credentials",
-    ensure => present,
-    require => File['.ivy2'],
-    content => "realm=Sonatype Nexus Repository Manager
-host=patch-nexus-a01.ihost.aol.com
-user=deploy
-password=P4tch-deploy!",
-  }
-
-  file { 'code':
-  	path => "/Users/${boxen_user}/code",
-  	ensure => directory,
-  }
-
-  exec {'kickass':
-	cwd => "/Users/${boxen_user}/code",
-	command => "git clone git@github.com:ckesurf/bomberman.git",
-	require => File['code'],
-  }
-
-
-
 
 
 
