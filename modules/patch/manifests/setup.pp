6991f4bf5a46f791b10b006d5d366a16d2f0214a
ckesurf <ckesurf@gmail.com>
2013-06-05 15:25:55 -0400

    modified override classes for server services. now declaring them and requiring them before the database creations

--- modules/patch/manifests/setup.pp
+++ modules/patch/manifests/setup.pp
@@ -10,15 +10,21 @@
   ############## PSQL stuff ############## 
 
   class patch::setup::stop_nginx inherits nginx {
-    Service['dev.nginx'] { ensure  => stopped}
+    Service['dev.nginx'] { ensure  => stopped }
   }   
 
-  class patch::setup::stop_psqlserver inherits postgresql {
-    Service['dev.postgresql'] { ensure  => stopped}
+  class patch::setup::stop_psql inherits postgresql {
+    Service['dev.postgresql'] { ensure  => stopped }
   }
+
+  include patch::setup::stop_nginx
+  include patch::setup::stop_psql
+
   exec { 'patchy db':
     command => "createdb -U patchy patchy -h localhost",
-  }
+    requires => [ Class['patch::setup::stop_nginx'], 
+                  Class['include patch::setup::stop_psql'] ],
+  } ->
   exec { 'patchy_test db':
     command => "createdb -U patchy patchy_test -h localhost",
   } ->
